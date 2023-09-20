e67454327646aeeb046aebf40a864969cb59339f
simpleflowgui <145459706+simpleflowgui@users.noreply.github.com>
2023-09-20 08:08:36 +0800

    Update testingterminal.py

--- backend/testingterminal.py
+++ backend/testingterminal.py
@@ -1,5 +1,8 @@
 import subprocess,webbrowser,os
 
 webbrowser.open("http://localhost:5173/")
-subprocess.run(["npm", "run","dev"], cwd=f"{os.path.abspath(os.path.join(os.getcwd(), os.pardir))}/my-react-flow-app")
+try:
+  subprocess.run(["npm", "run","dev"], cwd=f"{os.path.abspath(os.path.join(os.getcwd(), os.pardir))}/my-react-flow-app")
+except:
+  subprocess.run(["npm", "run","dev"], cwd=f"{os.path.abspath(os.path.join(os.getcwd(), os.pardir))}\my-react-flow-app",shell=True)
 
